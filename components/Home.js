c1ae90d1d55a955bface002a488b15b8c7329d95
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2021-05-28 12:33:42 +0200

    Add Home page

---
+++ components/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Text>Home</Text>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
