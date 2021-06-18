388f994aa4d3354afe85dda89756408735b72a20
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2021-06-18 11:53:54 +0200

    Update style in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,13 +10,16 @@ import {
 export default class Home extends Component {
   render() {
     return (
-      <View>
-        <Button
+      <View style={styles.container}>
+        <View style={styles.child}>
+          <Button
             title="Consult"
             onPress={() =>
               this.props.navigation.navigate('Consult')
             }
           />
+        </View>
+        <View style={styles.child}>
           <Button
             title="Report"
             onPress={() =>
@@ -24,10 +27,23 @@ export default class Home extends Component {
             }
           />
         </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    justifyContent: "start",
+    flexDirection: "column",
+    paddingTop: "4em",
+  },
+  child: {
+    marginTop: "1em",
+    marginBottom: "1em",
+    marginRight: "1em",
+    marginLeft: "1em",
+    textAlign: "center",
+  },
 });
