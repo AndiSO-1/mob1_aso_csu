f493e46b9f6b0b37c043b77bfc3b9431d2fa6e5b
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2021-06-22 11:01:46 +0200

    Update unconfirmed schedule component

--- src/components/UnconfirmedSchedule.js
+++ src/components/UnconfirmedSchedule.js
@@ -4,15 +4,56 @@ import {
   StyleSheet,
   View,
   Button,
+  Text,
+  Picker,
+  TextInput,
 } from 'react-native';
 
 export default class UnconfirmedSchedule extends Component {
   constructor(props) {
     super(props);
 
-    console.log(this.props.data);
+    this.unknown = "Inconnu";
+    this.discuss = "A discuter";
+    this.confirm = "Confirmé";
 
-    this.state = {};
+    this.state = {
+      status: [this.unknown, this.discuss, this.confirm],
+      selected_status: this.whichStatus(this.props.reason),
+      reason: "",
+    };
+  }
+
+  formatDate = (val) => {
+    var d = new Date(val);
+
+    var dd = d.getDate();
+    var mm = d.getMonth() + 1;
+    var yyyy = d.getFullYear();
+
+    return ("0" + dd).slice(-2) + '/' + ("0" + mm).slice(-2) + '/' + yyyy;
+  }
+
+  whichStatus(val){
+    switch (val) {
+      case null:
+        return this.unknown;
+      break;
+      case 0:
+        return this.discuss;
+      break;
+      case 1:
+        return this.confirm;
+      break;
+      default:
+        return this.unknown;
+    }
+  }
+
+  updateStatus = (val) => {
+    this.setState({
+      selected_status: val
+    });
   }
 
   handleText(input, value) {
@@ -24,6 +65,22 @@ export default class UnconfirmedSchedule extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Text>{this.props.data.worktime.type}</Text>
+        <Text>{this.formatDate(this.props.data.date)}</Text>
+        <View>
+          <Picker selectedValue={this.state.selected_status} onValueChange={this.updateStatus}>
+            {
+              this.state.status.map(v =>
+              <Picker.Item key={v} label={v} value={v} />)
+            }
+          </Picker>
+        </View>
+        {this.state.selected_status == this.discuss ?
+          <View>
+            <Text>Raison (10-50 lettres):</Text>
+            <TextInput style={styles.input} onChangeText={(text) => this.handleText("reason", text)}/>
+          </View> : null
+        }
         <Button
           onPress={() => null}
           title="Enregister"
@@ -45,5 +102,11 @@ const styles = StyleSheet.create({
     marginBottom: "1em",
     marginRight: "1em",
     marginLeft: "1em",
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    textAlign: "center",
+    backgroundColor: "white",
   },
 });
