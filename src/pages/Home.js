052a73fdcfb4fa500b3b44e7392e5e121bd8e37c
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2021-06-22 10:18:07 +0200

    Display schedule button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,66 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+// Contexts
+import { LoginContext } from '../contexts/LoginContext'; // Perm login to change user status
+
+import manageException from "../utils";
+
+// Toast message
+import Toast from 'react-native-toast-message';
+
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getUnconfirmedWorkplans = this.getUnconfirmedWorkplans.bind(this);
+
+    this.api = 'http://127.0.0.1:8000/api/';
+
+    this.state = {
+      unconfirmed_workplans: 0,
+    };
+  }
+
+  async getUnconfirmedWorkplans(){
+    let token = this.context.token;
+
+    let unconfirmed_workplans = await fetch(this.api + 'unconfirmedworkplans', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + token,
+      },
+    })
+    .then(function(response) {
+      if(response.ok) {
+        return response.json();
+      }
+      else {
+        Toast.show(manageException(response.status));
+      }
+    })
+    .then(function(data){
+      return data;
+    })
+    .catch(function(error) {
+      Toast.show(manageException());
+    });
+
+    this.setState({
+      unconfirmed_workplans: unconfirmed_workplans ? unconfirmed_workplans.length : 0,
+    });
+  }
+
+  componentDidMount () {
+    this.getUnconfirmedWorkplans();
+  }
+
+  useEffect  () {
+    this.state = {
+      unconfirmed_workplans: 0,
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -28,19 +87,24 @@ export default class Home extends Component {
             }
           />
         </View>
-        <View style={styles.child}>
-          <TouchableOpacity
-            onPress={() =>
-              this.props.navigation.navigate('Schedule')
-            }
-            style={styles.button}>
-            <Text>{"Horaires à valider"}</Text>
-          </TouchableOpacity>
-        </View>
+        {this.state.unconfirmed_workplans ?
+          <View style={styles.child}>
+            <TouchableOpacity
+              onPress={() =>
+                this.props.navigation.navigate('Schedule')
+              }
+              style={styles.button}>
+              <Text style={styles.button_text}>{"HORAIRES À VALIDER"}<Text style={styles.nb_schedules}>{this.state.unconfirmed_workplans}</Text></Text>
+            </TouchableOpacity>
+          </View> : null
+        }
+
       </View>
     );
   }
 }
+
+Home.contextType = LoginContext;
 
 const styles = StyleSheet.create({
   container: {
@@ -60,5 +124,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "rgb(33, 150, 243)",
     padding: 10
+  },
+  button_text:{
+    color: "white",
+  },
+  nb_schedules:{
+    backgroundColor: "red",
+    color: "white",
+    marginLeft: "1em",
+    paddingRight: "1em",
+    paddingLeft: "1em",
   },
 });
